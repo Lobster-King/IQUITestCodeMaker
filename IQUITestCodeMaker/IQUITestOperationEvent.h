f7e4f609fec6bbb6d91235daacbaadc0410df9cc
Lobster-King <zhiwei.geek@gmail.com>
2018-08-15 19:33:39 +0800

    locateStrategy

--- IQUITestCodeMaker/IQUITestOperationEvent.h
+++ IQUITestCodeMaker/IQUITestOperationEvent.h
@@ -22,10 +22,18 @@ typedef NS_ENUM(NSInteger,IQEventType ) {
     IQEventEndCode      = 9,/*结束录制事件*/
 };
 
+typedef NS_ENUM(NSInteger,IQElementLocateStrategy ) {
+    IQElementLocateByIdentifier     = 0,/*通过控件identifier定位元素*/
+    IQElementLocateByCoordinate     = 1,/*通过坐标定位元素*/
+    IQElementLocateByPredicate      = 2,/*通过谓词定位元素*/
+};
+
 @interface IQUITestOperationEvent : NSObject
 
+@property (nonatomic, assign) IQElementLocateStrategy *locateStrategy;/*定位策略，默认IQElementLocateByIdentifier*/
 @property (nonatomic, assign) IQEventType eventType;
 @property (nonatomic, copy)   NSString *identifier;
 @property (nonatomic, copy)   NSString *value;
+@property (nonatomic, assign) CGPoint touchPoint;
 
 @end
